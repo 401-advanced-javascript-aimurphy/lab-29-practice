f44bf93bc2682ccbf7a74506111b2c464558f93e
Aileen Murphy <ai.murf2@gmail.com>
2019-09-18 13:38:35 -0700

    revisited and refactored with help from sonia

--- src/components/card.js
+++ src/components/card.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import {If, When, Unless} from './if/index.js';
+
 
 function Card(props) {
-  return <div>{props.children}</div>;
+  const card=props.content || [];
+  return (<div>
+    {/* @815pm 17sept */}
+      <When condition={card.title}>
+              <header><h3>{card.title}</h3></header>
+              </When>
+              <div className="content">{card.copy}</div>
+              <figure>
+                <img src={card.media.href} alt={card.media.alt} />
+                <figcaption>{card.media.title}</figcaption>
+              </figure>
+              <nav className="links">
+                <ul>
+                  {card.links.map( (link,i) => (
+                    <li key={i}><a className={link.type} href={link.href} title={link.title}>{link.title}</a></li>
+                  ))}
+                </ul>
+              </nav>
+    {props.children}</div>);
 }
 
 export default Card;
--- src/components/deck.js
+++ src/components/deck.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import {If, When, Unless} from './if/index.js';
+
 
 function Deck(props) {
-  return <div>{props.children}</div>;
+const deck=props.deck||[];
+  return (
+    <div>
+      <When condition={deck.title}>
+        <header><h2>{deck.title}</h2></header>
+      </When>
+      {props.children}
+      </div>
+  );
 }
 
 export default Deck;
